d0779e55912bd820d9892c5c53ec96189a255b5a
Caleb Aston <caleb.k.aston@gmail.com>
2016-10-07 07:09:17 -0600

    Successfully added a router and navigation links from the search page, to the results page

--- app/main.js
+++ app/main.js
@@ -2,15 +2,25 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 var platform_1 = require("nativescript-angular/platform");
 var core_1 = require("@angular/core");
+var router_1 = require("nativescript-angular/router");
 var app_component_1 = require("./app.component");
+var home_component_1 = require("./home/home.component");
+var search_results_component_1 = require("./search-results/search-results.component");
 var AppComponentModule = (function () {
     function AppComponentModule() {
     }
     AppComponentModule = __decorate([
         core_1.NgModule({
-            declarations: [app_component_1.AppComponent],
+            declarations: [app_component_1.AppComponent,
+                home_component_1.HomeComponent,
+                search_results_component_1.SearchResultsComponent],
             bootstrap: [app_component_1.AppComponent],
-            imports: [platform_1.NativeScriptModule],
+            imports: [platform_1.NativeScriptModule,
+                router_1.NativeScriptRouterModule,
+                router_1.NativeScriptRouterModule.forRoot([
+                    { path: '', component: home_component_1.HomeComponent },
+                    { path: 'search-results', component: search_results_component_1.SearchResultsComponent }
+                ])],
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponentModule);
