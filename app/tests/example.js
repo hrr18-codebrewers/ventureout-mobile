3c754d8fa04a3798dd5bba1c1d8421627c45ee4d
Caleb Aston <caleb.k.aston@gmail.com>
2016-10-12 14:10:30 -0600

    Added in Mocha Testing

---
+++ app/tests/example.js
@@ -0,0 +1,23 @@
+// Import Components for Testing
+var reflect = require("reflect-metadata");
+var Home = require("../home/home.component");
+
+
+describe('VO-Mobile', function () {
+	describe('Home', function () {
+		it('should return -1 when the value is not present', function () {
+      assert.equal(-1, -1);
+      assert.equal(-1, -1);
+		});
+	});
+  describe('SearchService', function () {
+    it('should return -1 when the value is not present', function () {
+
+    });
+  });
+  describe('SearchResults', function () {
+    it('should return -1 when the value is not present', function () {
+
+    });
+  });
+});
